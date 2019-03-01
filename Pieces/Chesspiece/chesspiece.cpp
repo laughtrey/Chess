b3253ceecb357dcf2dc99d1bc7868c25dfb66ed6
laughtrey <laughtrey@gmail.com>
2019-03-01 03:10:25 -0800

    I think we can make an inheritor class from this Chesspiece class.

---
+++ Pieces/Chesspiece/chesspiece.cpp
@@ -0,0 +1,26 @@
+class Chesspiece {
+private:
+  int m_max_spaces = 7;
+  bool move_diagonal,move_vertical,move_horizontal;
+
+public:
+  Chesspiece(int max_spaces,bool move_diag,bool move_vert,bool move_horiz);
+};
+Chesspiece::Chesspiece(int max_spaces,bool move_diag,bool move_vert,bool move_horiz)
+{
+    m_max_spaces = max_spaces;
+    move_diagonal = move_diag;
+    move_vertical = move_vert;
+    move_horizontal = move_horiz;
+
+    /**
+    *==Prototype Objects==
+    *Chesspiece Queen(7,true,true,true);
+    *Chesspiece Pawn(2,false,true,false);
+    *==Eventually, with inheritance==
+    *Queen wQ(7,true,true,true);
+    *Pawn a2(2,false,true,false);
+    *Pawn b2(2,false,true,false);
+    *Pawn c2(2,false,true,false);
+    */
+}
