4075c53a9d406ed1167edf2c52607e2cf29a59d5
laughtrey <laughtrey@gmail.com>
2019-03-01 21:03:18 -0800

    I forgot to actually create the objects.

--- ChessBoard/board.cpp
+++ ChessBoard/board.cpp
@@ -13,22 +13,22 @@ Board()
  */
 void create_board() {
   chesspiece playing_field[8][8];
-  playing_field[0][0] = new wR(7,false,true,true);
-  playing_field[0][1] = new wN(3,false,true,true);
-  playing_field[0][2] = new wB(7,true,false,false);
-  playing_field[0][3] = new wK(1,true,true,true);
-  playing_field[0][4] = new wQ(7,true,true,true);
-  playing_field[0][5] = new wB(7,true,false,false);
-  playing_field[0][6] = new wN(3,true,false,false);
-  playing_field[0][7] = new wR(7,false,true,true);
-  playing_field[1][0] = new wP(2,false,true,false);
-  playing_field[1][1] = new wP(2,false,true,false);
-  playing_field[1][2] = new wP(2,false,true,false);
-  playing_field[1][3] = new wP(2,false,true,false);
-  playing_field[1][4] = new wP(2,false,true,false);
-  playing_field[1][5] = new wP(2,false,true,false);
-  playing_field[1][6] = new wP(2,false,true,false);
-  playing_field[1][7] = new wP(2,false,true,false);
+  playing_field[0][0] = new Rook wR(7,false,true,true);
+  playing_field[0][1] = new Knight wN(3,false,true,true);
+  playing_field[0][2] = new Bishop wB(7,true,false,false);
+  playing_field[0][3] = new King wK(1,true,true,true);
+  playing_field[0][4] = new Queen wQ(7,true,true,true);
+  playing_field[0][5] = new Bishop wB(7,true,false,false);
+  playing_field[0][6] = new Knight wN(3,true,false,false);
+  playing_field[0][7] = new Rook wR(7,false,true,true);
+  playing_field[1][0] = new Pawn wP(2,false,true,false);
+  playing_field[1][1] = new Pawn wP(2,false,true,false);
+  playing_field[1][2] = new Pawn wP(2,false,true,false);
+  playing_field[1][3] = new Pawn wP(2,false,true,false);
+  playing_field[1][4] = new Pawn wP(2,false,true,false);
+  playing_field[1][5] = new Pawn wP(2,false,true,false);
+  playing_field[1][6] = new Pawn wP(2,false,true,false);
+  playing_field[1][7] = new Pawn wP(2,false,true,false);
 
 
 
