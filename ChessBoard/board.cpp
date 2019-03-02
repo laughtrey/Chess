ad63da839117b790e5b542509cef3f000f8268ed
laughtrey <laughtrey@gmail.com>
2019-03-01 20:58:53 -0800

    added actual constructor parameters

--- ChessBoard/board.cpp
+++ ChessBoard/board.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include "board.h"
 #include "square.h"
+#include "chesspiece.cpp"
 Board()
 {
 
@@ -12,22 +13,22 @@ Board()
  */
 void create_board() {
   chesspiece playing_field[8][8];
-  playing_field[0][0] = new wR();
-  playing_field[0][1] = new wN();
-  playing_field[0][2] = new wB();
-  playing_field[0][3] = new wK();
-  playing_field[0][4] = new wQ();
-  playing_field[0][5] = new wB();
-  playing_field[0][6] = new wN();
-  playing_field[0][7] = new wR();
-  playing_field[1][0] = new wP();
-  playing_field[1][1] = new wP();
-  playing_field[1][2] = new wP();
-  playing_field[1][3] = new wP();
-  playing_field[1][4] = new wP();
-  playing_field[1][5] = new wP();
-  playing_field[1][6] = new wP();
-  playing_field[1][7] = new wP();
+  playing_field[0][0] = new wR(7,false,true,true);
+  playing_field[0][1] = new wN(3,false,true,true);
+  playing_field[0][2] = new wB(7,true,false,false);
+  playing_field[0][3] = new wK(1,true,true,true);
+  playing_field[0][4] = new wQ(7,true,true,true);
+  playing_field[0][5] = new wB(7,true,false,false);
+  playing_field[0][6] = new wN(3,true,false,false);
+  playing_field[0][7] = new wR(7,false,true,true);
+  playing_field[1][0] = new wP(2,false,true,false);
+  playing_field[1][1] = new wP(2,false,true,false);
+  playing_field[1][2] = new wP(2,false,true,false);
+  playing_field[1][3] = new wP(2,false,true,false);
+  playing_field[1][4] = new wP(2,false,true,false);
+  playing_field[1][5] = new wP(2,false,true,false);
+  playing_field[1][6] = new wP(2,false,true,false);
+  playing_field[1][7] = new wP(2,false,true,false);
 
 
 
